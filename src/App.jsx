8f1a6100b926e3a8e4ced3bee583cf99e3749ea7
KeerthanaS2002 <keerthanas.sk2004@gmail.com>
2025-01-06 12:09:02 +0530

    uploading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+/*import React from 'react';
 import SlamBook from './SlamBook';
-
 function App() {
   return (
     <div className="App">
@@ -8,6 +7,23 @@ function App() {
     </div>
   );
 }
+export default App;*/
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Details from "./Details";
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/details/:id" element={<Details />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './App.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Apps from "./Apps";
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+          <Apps />      
+  </StrictMode>,
+)
